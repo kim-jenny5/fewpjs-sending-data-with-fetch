0811f96e9b71a4e1f579b8761e226214be27772b
Jenny Kim <kimj1905@gmail.com>
2021-09-01 16:47:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const url = "http://localhost:3000/users"
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }).then(resp => resp.json())
+        .then(function (obj) {
+            document.body.innerHTML = Object.values(obj)[0]
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message;
+        })
+}
