a7011198b897c522fd87be448e5f99ae383869d9
Jenny Kim <kimj1905@gmail.com>
2021-09-01 16:46:54 -0400

    commit tree f3802b404de3ba439ceba02eddbce49a30a10438

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const url = "http://localhost:3000/users"
+    // return fetch(url, postingObject)
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }).then(resp => resp.json())
+        .then(function (obj) {
+            // debugger
+            // console.log(Object.values(obj)[0])
+            document.body.innerHTML = Object.values(obj)[0]
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message;
+        })
+
+    // const postingObject = {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         name: userName,
+    //         email: userEmail
+    //     })
+    // }
+}
